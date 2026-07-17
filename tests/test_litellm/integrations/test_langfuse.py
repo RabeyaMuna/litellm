290f5ab41059f6689a2e1266c7c502f1c51daafe
RabeyaMuna <rabeya.khatun.muna@g.bracu.ac.bd>
2026-07-17 09:58:08 -0400

    393

--- tests/test_litellm/integrations/test_langfuse.py
+++ tests/test_litellm/integrations/test_langfuse.py
@@ -1,6 +1,7 @@
 import json
 import os
 import sys
+from types import SimpleNamespace
 from typing import Optional
 
 # Adds the grandparent directory to sys.path to allow importing project modules
@@ -15,19 +16,36 @@ import litellm
 from litellm.integrations.langfuse.langfuse import LangFuseLogger
 
 
-def test_max_langfuse_clients_limit():
+def test_max_langfuse_clients_limit(monkeypatch):
     """
     Test that the max langfuse clients limit is respected when initializing multiple clients
     """
+    class MockLangfuse:
+        def __init__(self, **kwargs):
+            self.kwargs = kwargs
+            self.client = SimpleNamespace(
+                projects=SimpleNamespace(
+                    get=lambda: SimpleNamespace(
+                        data=[SimpleNamespace(id="test-project-id")]
+                    )
+                )
+            )
+
+    mock_langfuse_module = SimpleNamespace(
+        Langfuse=MockLangfuse,
+        version=SimpleNamespace(__version__="2.6.0"),
+    )
+    monkeypatch.setitem(sys.modules, "langfuse", mock_langfuse_module)
+
     # Set max clients to 2 for testing
     with patch(
         "litellm.integrations.langfuse.langfuse.MAX_LANGFUSE_INITIALIZED_CLIENTS", 2
     ):
         # Reset the counter
-        litellm.initialized_langfuse_clients = 0
+        monkeypatch.setattr(litellm, "initialized_langfuse_clients", 0)
 
         # First client should succeed
-        logger1 = LangFuseLogger(
+        LangFuseLogger(
             langfuse_public_key="test_key_1",
             langfuse_secret="test_secret_1",
             langfuse_host="https://test1.langfuse.com",
@@ -35,7 +53,7 @@ def test_max_langfuse_clients_limit():
         assert litellm.initialized_langfuse_clients == 1
 
         # Second client should succeed
-        logger2 = LangFuseLogger(
+        LangFuseLogger(
             langfuse_public_key="test_key_2",
             langfuse_secret="test_secret_2",
             langfuse_host="https://test2.langfuse.com",
@@ -44,7 +62,7 @@ def test_max_langfuse_clients_limit():
 
         # Third client should fail with exception
         with pytest.raises(Exception) as exc_info:
-            logger3 = LangFuseLogger(
+            LangFuseLogger(
                 langfuse_public_key="test_key_3",
                 langfuse_secret="test_secret_3",
                 langfuse_host="https://test3.langfuse.com",
